6b6e2b1588e1bf8c3c14ffdc111f486d1cfba9e8
alti21 <altemush@gmail.com>
2020-01-25 15:54:12 -0800

    Added mist background and changing city removes old city's info

--- src/js/models/Weather.js
+++ src/js/models/Weather.js
@@ -14,14 +14,12 @@ export default class Weather
     {
         try 
         {
-            //const res = await fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=fe621362f3067a7385ceb99bd2accef5');
-            //const res = await axios('https://api.openweathermap.org/data/2.5/weather?id=' + this.id + '&appid=fe621362f3067a7385ceb99bd2accef5');
             const res = await axios('https://api.openweathermap.org/data/2.5/weather?q=' + this.name + '&appid=fe621362f3067a7385ceb99bd2accef5');
-            //const data = await res.json();
-            //return data;
             console.log(res);
             console.log(res.data);
             this.weatherDesc = res.data.weather[0].description;
+            this.icon = `http://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`;
+            //http://openweathermap.org/img/wn/10d@2x.png
         }
         catch(error)
         {   
--- src/js/views/weatherView.js
+++ src/js/views/weatherView.js
@@ -1,6 +1,35 @@
 export const renderWeather = (weather) => {
+    //to display the current weather on the UI
     const markup = `
-    <p> ${weather.weatherDesc} </p>
+    <div class="results__container">
+        <p class="results__weather--info"> ${weather.weatherDesc} </p>
+        <img class="results__weather--info" src=${weather.icon}>
+    </div>
     `;
-    document.querySelector('.search__form').insertAdjacentHTML('beforeend', markup);
+    if(document.querySelector('.results__weather').contains( document.querySelector('.results__container') ))
+    {
+        document.querySelector('.results__weather').removeChild(document.querySelector('.results__container'));
+    }
+   // document.querySelector('.results__weather').replaceChild()
+    document.querySelector('.results__weather').insertAdjacentHTML('afterbegin', markup);
+    //TODO remove current weather info if user searches another city
+    //remove children of div with class = results__weather
+
+    //change background depending on weather
+    if(weather.weatherDesc === 'haze' || weather.weatherDesc === 'mist')
+    {
+        document.querySelector('.container').classList.add('background__mist');
+       
+        document.querySelector('.mist__item--1').classList.add('mist');
+        document.querySelector('.mist__item--2').classList.add('mist');
+        document.querySelector('.mist__item--3').classList.add('mist');
+        document.querySelector('.mist__item--4').classList.add('mist');
+        document.querySelector('.mist__item--5').classList.add('mist');
+
+        document.querySelector('.mist__item--1-2').classList.add('mist');
+        document.querySelector('.mist__item--2-2').classList.add('mist');
+        document.querySelector('.mist__item--3-2').classList.add('mist');
+        document.querySelector('.mist__item--4-2').classList.add('mist');
+        document.querySelector('.mist__item--5-2').classList.add('mist');
+    }
 }
